767db9945a0a2c19297e68d8108468907898afe3
EzequielRomero7613 <romeroeze7613@gmail.com>
2022-10-31 01:01:42 -0300

    update 31/10

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+//boton para ir arriba
+document.getElementById("arriba").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+  });
+});
 
+//boton hamburguesa
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-document.getElementById('arriba')
-.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        
-    })
-})
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
-
-document.getElementById('btnwpp')
-.addEventListener('click', function(){
-})
+//boton de wpp
